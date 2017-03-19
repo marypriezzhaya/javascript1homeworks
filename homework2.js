9a24c98e44e660997f7f5dfe94c34d2c7d1985a3
marypriezzhaya <ives@ives-de-gris.ru>
2017-03-19 21:53:19 +0300

    6 tasks ready

--- homework2.js
+++ homework2.js
@@ -26,3 +26,135 @@ else {
 }
 
 /*  Task 4  */
+//если все сразу, то
+var a = 8;
+switch ( a ) {
+  case 0 :
+  alert ( "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 1 :
+  alert ( "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 2 :
+  alert ( "2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 3 :
+  alert ( "3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 4 :
+  alert ( "4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 5 :
+  alert ( "5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 6 :
+  alert ( "6, 7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 7 :
+  alert ( "7, 8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 8 :
+  alert ( "8, 9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 9 :
+  alert ( "9, 10, 11, 12, 13, 14, 15" );
+  break;
+  case 10 :
+  alert ( "10, 11, 12, 13, 14, 15" );
+  break;
+  case 11 :
+  alert ( "11, 12, 13, 14, 15" );
+  break;
+  case 12 :
+  alert ( "12, 13, 14, 15" );
+  break;
+  case 13 :
+  alert ( "13, 14, 15" );
+  break;
+  case 14 :
+  alert ( "14, 15" );
+  break;
+  case 15 :
+  alert ( "15" );
+  break;
+}
+// если по очереди :
+var a = 8;
+switch ( a ) {
+  case 0 :
+  alert ( "0" );
+  case 1 :
+  alert ( "1" );
+  case 2 :
+  alert ( "2" );
+  case 3 :
+  alert ( "3" );
+  case 4 :
+  alert ( "4" );
+  case 5 :
+  alert ( "5" );
+  case 6 :
+  alert ( "6" );
+  case 7 :
+  alert ( "7" );
+  case 8 :
+  alert ( "8" );
+  case 9 :
+  alert ( "9" );
+  case 10 :
+  alert ( "10" );
+  case 11 :
+  alert ( "11" );
+  case 12 :
+  alert ( "12" );
+  case 13 :
+  alert ( "13" );
+  case 14 :
+  alert ( "14" );
+  case 15 :
+  alert ( "15" );
+}
+
+/* Task 5 */
+
+  function addition ( a, b ) {
+    var result = a + b;
+    return result;
+  }
+
+  function substraction ( a, b ) {
+    var result = a - b;
+    return result;
+  }
+
+  function multiplication ( a, b ) {
+    var result = a * b;
+    return result;
+  }
+
+  function division ( a, b ) {
+    var result = a / b;
+    return result;
+  }
+
+  /* Task 6 */
+  function mathOperation ( a, b, operation ) {
+    var res;
+  switch ( operation ) {
+    case 'add':
+    res = addition( a, b );
+    break;
+    case 'substract':
+    res = substraction( a, b );
+    break;
+    case 'multiply':
+    res = multiplication( a, b );
+    break;
+    case 'divide':
+    res = division( a, b );
+    break;
+  }
+  return res;
+    }
+var weGotIt = mathOperation ( 3, 2, 'add' );
+alert ( weGotIt );
